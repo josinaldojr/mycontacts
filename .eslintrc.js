bb735f2c7281409d801dc1afea7965b7a06a2a2c
Josinaldo Júnior <josinaldo.pontes@dcx.ufpb.br>
2022-01-18 19:01:23 -0300

    feat: create project

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 13,
+  },
+  rules: {
+    'linebreak-style': 0,
+    'class-methods-use-this': 'off',
+    'no-promise-executor-return': 'off',
+    'consistent-return': 'off',
+    camelcase: 'off',
+    'no-unused-vars': ['error', { argisIgnorePattern: 'next' }],
+  },
+};
